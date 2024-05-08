2b54e922502e069cb584330ac4703aef0f35485f
Crissomar1 <109483254+Crissomar1@users.noreply.github.com>
2024-05-08 02:43:27 -0600

    application and model

---
+++ Email_Encoder.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import random
+import os
+import mailbox
+from collections import Counter
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+def flatten_payload(email):
+    email_text = email.get_payload()
+    if isinstance(email_text, list):
+        return ' '.join(flatten_payload(part) for part in email_text)
+    else:
+        return email_text
+
+def preprocess_email(email):
+    email_text = email.get_payload()
+    email_text = flatten_payload(email)
+    # 1. Conversión a minúsculas:
+    email_text = email_text.lower()
+
+    # 2. Eliminación de caracteres especiales y URLs:
+    email_text = re.sub(r'[^a-z0-9\s]', ' ', email_text)  # Sustituye caracteres especiales
+    email_text = re.sub(r'http\S+', ' ', email_text)  # Elimina URLs
+
+    # 3. Tokenización (separa el texto en palabras):
+    words = email_text.split()
+
+    # 4. Eliminación de stopwords (palabras de enlace frecuentes):
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+
+    # 5. Lematización (reduce palabras a su raíz):
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word) for word in words]
+
+    return ' '.join(words)
+
+def one_hot_encode(text, vocab_dict):
+    # Convierte el texto en una lista de palabras preprocesadas
+    words = preprocess_email(text)
+
+    # Crea un vector de ceros del tamaño del vocabulario
+    encoded_email = np.zeros(len(vocab_dict))
+
+    # Asigna 1 a la posición correspondiente a cada palabra en el vocabulario
+    for word in words:
+        if word in vocab_dict:
+            encoded_email[vocab_dict[word]] += 1
+
+    return encoded_email
+
+def read_vocab() -> list:
+    if os.path.exists('vocab.txt'):
+        with open('vocab.txt', 'r') as f:
+            vocab = [line.strip() for line in f]
+        vocab_dict = {word: i for i, word in enumerate(vocab)}
+    else:
+        vocab_dict = {}
+    return vocab_dict
+
+def encode_email(dir, vocab_dict):
+    # Carga el correo electrónico
+    email = mailbox.mbox(dir)
+    encoded_email = one_hot_encode(email, vocab_dict)
+    return encoded_email
---
+++ Final.py
@@ -0,0 +1,58 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from Main_ui import Ui_dialog
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from PyQt5 import QtGui
+import Email_Encoder
+
+from tensorflow import keras
+
+
+class Dialog(QMainWindow, Ui_dialog):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+        #randominzar pesos
+        self.inicializar()
+
+    def inicializar(self):
+        # cargar modelo
+        self.model = keras.models.load_model('spam_model.keras')
+        if self.model:
+            self.label_Error.setText('Modelo cargado')
+        else:
+            self.label_Error.setText('Error al cargar modelo')
+        # read vocab
+        self.vocab_dict = Email_Encoder.read_vocab()
+        if self.vocab_dict:
+            self.label_Error.setText('Vocabulario cargado')
+        else:
+            self.label_Error.setText('Error al cargar vocabulario')
+
+    def on_pushButton_Load_clicked(self):
+        #abrir archivo
+        filename = QFileDialog.getOpenFileName(self, 'Open Mail File', '')
+        if filename[0]:
+            #encode email
+            email = Email_Encoder.read_email(filename[0])
+            encoded_email = Email_Encoder.one_hot_encode(email, self.vocab_dict)
+            #predict
+            prediction = self.model.predict(encoded_email)
+            self.label_Label.setText("Spam" if prediction[0] > 0.5 else "No Spam")
+            self.label_Error.setText('')
+
+
+    def accept(self):
+        pass
+
+    def reject(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = Dialog()
+    form.show()
+    sys.exit(app.exec_())
